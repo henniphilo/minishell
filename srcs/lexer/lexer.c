c38e63b07a017d591a5406da2bcd440b5849682a
pebencze <petra.bncz@gmail.com>
2024-04-15 19:16:37 +0200

    handle quotes lexing

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -20,17 +20,38 @@ static t_lexer	*new_list(t_type t, char *s, bool q)
 	return (new_node);
 }
 
+static void	ft_lstadd_back(t_lexer **lst, t_lexer *new)
+{
+	t_lexer	*pos;
+
+	pos = ft_lstlast(*lst);
+	if (!pos)
+		*lst = new;
+	else
+	{
+		pos->next = new;
+		new->previous = pos;
+	}
+}
+
 static char	*handle_quotes(char *tmp_buf, t_lexer *tokens)
 {
-	char	i;
-	char	*s;
 	t_lexer	*node;
+	char	*s;
+	char	i;
 
-	i++;
+	i = 0;
 	while (*tmp_buf != tmp_buf[i])
 		i++;
-	s = ft_substr((const char *)(tmp_buf + 1), 0, (tmp_buf - ));
-	return (++tmp_buf);
+	s = ft_substr((const char *)(tmp_buf), 1, i);
+	if (!s)
+		return (NULL);
+	printf("%s", s); //test
+	node = new_list(WORD, s, (*tmp_buf == '\''));
+	if (!node)
+		return (NULL);
+	list_add_back(tokens, node);
+	return (tmp_buf + i + 1);
 }
 
 int	lexer(t_data *data)
@@ -39,19 +60,21 @@ int	lexer(t_data *data)
 
 	//buf = ft_strtrim(data->buf, " ");
 	tmp_buf = data->buf;
-	data->tokens = new_list(0, 0, 0);
-	if (!data->tokens)
-		return (error_int(ALLOC_ERR));
+	//data->tokens = new_list(0, 0, 0);
+	//if (!data->tokens)
+	//	return (error_int(ALLOC_ERR));
 	while (*tmp_buf)
 	{
 		while (*tmp_buf == 32)
 			tmp_buf++;
 		if (*tmp_buf == '\"' || *tmp_buf == '\'')
-			tmp_buf = handle_quotes(tmp_buf);
+			tmp_buf = handle_quotes(tmp_buf, data->tokens);
 		else if (ft_strchr("<>|", *tmp_buf))
 			//tmp_buf = handle_meta(tmp_buf, );
 		else if (*tmp_buf)
 			//tmp_buf = handle_words(tmp_buf, data->tokens);
+		if (!tmp_buf)
+			return (error_int(ALLOC_ERR));
 	}
 	return (0);
 }
