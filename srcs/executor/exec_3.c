e48506c15bb2292493576984c09bd08d8a11d5b4
Henni <henni.wie@gmail.com>
2024-04-15 17:43:40 +0200

    am dup arbeiten

--- srcs/executor/exec_3.c
+++ srcs/executor/exec_3.c
@@ -1,28 +1,50 @@
 #include "../../incl/minishell.h"
 
 
+int		count_commands(t_data *shell)
+{
+	int		cmd_count;
+
+	cmd_count = 0;
+	while (shell->arguments[cmd_count] != NULL)
+		cmd_count++;
+	printf("command count is: %d\n");
+	return(cmd_count);
+}
+
+
 //hier weiternachen
-int	launch_shell(t_data *shell)
+int	execute_shell(t_data *shell)
 {
 	pid_t	pid;
+	int		cmd_count;
 
 	// if (pipe(fd) < 0)
 	// 	perror("Error\n");
-
-	pid = fork();
-
-	if (pid < 0)
+	cmd_count = count_commands(shell);
+	if(cmd_count == 1)
 	{
-		perror("fork problem");
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork problem");
+		}
+		if (pid == 0) //ist in child
+			env_execute (shell, shell->env);
+		else
+		{
+			//parent process
+			printf("Elternprozess: PID = %d, Kindprozess-PID = %d\n", getpid(), pid);
+		}
+		waitpid(pid, NULL, 0);
 	}
-	if (pid == 0) //ist in child
-		execute (shell, shell->env);
 	else
-	{
-		//parent process
-		printf("Elternprozess: PID = %d, Kindprozess-PID = %d\n", getpid(), pid);
-	}
-	waitpid(pid, NULL, 0);
-
+		execute_pipes(shell);
 	return (1);
 }
+
+int	execute_pipes(t_data *shell)
+{
+	
+}
+
