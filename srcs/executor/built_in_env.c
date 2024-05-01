d77695764424cf55c222e30e9797d835681e16be
Henni <henni.wie@gmail.com>
2024-05-01 15:49:06 +0200

    little change

--- srcs/executor/built_in_env.c
+++ srcs/executor/built_in_env.c
@@ -16,26 +16,32 @@ char	*find_in_env(char *to_find)
 
 void		bi_unset(t_data *shell)
 {
-	t_environ	*head;
+	t_environ	*prev;
 	t_environ	*begin;
 	t_environ	*remove;
-	int			len;
+	t_environ	*head;
+	int			len; //len spaeter vlt. wegmachen
 
-	head = shell->env_list;
-	begin = head;
+	begin = shell->env_list;
+	head = begin;
+	prev = NULL;
 	len = ft_strlen(shell->toex[1]);
-	if(shell->toex[1] != NULL)
+	if (shell->toex[1] != NULL)
 	{
 		while(head != NULL)
 		{
-			if(ft_strncmp(head->name, shell->toex[1], len) == 0)
+			if(ft_strncmp(head->name, shell->toex[1], len + 1) == 0)
 			{
 				if(shell->toex[2] == NULL)
 				{
 					remove = head;
-					printf(">this to remove: %s=%s < ", remove->name, remove->value);
-					head = head->next;
-					free(remove);
+		//			printf(">this to remove: %s=%s < \n", remove->name, remove->value);
+					if (prev)
+						prev->next = head->next;
+					else
+						begin = head->next;
+		//			printf(">this is tmp: %s=%s < \n", head->name, head->value);
+					delone_env_list(remove);
 					break ;
 				}
 				else
@@ -44,12 +50,11 @@ void		bi_unset(t_data *shell)
 					break ;
 				}
 			}
+			prev = head;
 			head = head->next;
 		}
 	}
-	else
-		perror("Error. not enough arguments for unset\n");
-	print_env(begin);
+//	print_env(begin);
 }
 
 t_environ	*replace_value(t_environ *list_ptr, char *replace)
